ddfe18c2caf268110b3429949f2c6d8b83f0bdd0
ruthjohnson95 <ruthjohnson2013@gmail.com>
2017-05-21 23:30:04 -0700

    update

--- lab4b.c
+++ lab4b.c
@@ -1,18 +1,24 @@
-#include "mraa/aio.h"
+#include "mraa.h"
 #include <math.h>
 
 const int B = 4275;
 const int R0 = 100;
+const int button_pin = 3; 
 
 int main()
 {
   mraa_aio_context adc_a0;
+  mraa_gpio_context gpio;
+
   uint16_t adcValue = 0;
   float adc_value_float = 0.0;
   adc_a0 = mraa_aio_init(0);
   if (adc_a0 == NULL) {
     return 1;
   }
+
+  gpio = mraa_gpio_init(button_pin); 
+  mraa_gpio_dir(gpio, MRAA_GPIO_IN);
 
   for (;;) {
     adcValue = mraa_aio_read(adc_a0);
@@ -22,7 +28,9 @@ int main()
 
     float temp  = 1.0/(log(R/100000.0)/B+1/298.15)-273.15;
 
-    printf ("The temperature is %0.2f degree Celcius\n", temp);
+    //    printf ("The temperature is %0.2f degree Celcius\n", temp);
+    fprintf(stdout, "Gpio is %d\n", mraa_gpio_read(gpio));
+
 
   }
   mraa_aio_close(adc_a0);
